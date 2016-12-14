917104c1dc2531fa36d9481622b6f8cd7bd8bd40
Tamas Eros <tamas.eros1@gmail.com>
2016-12-14 21:44:02 +0000

    LedControl setColor tescases added

--- test/TestLedControl.cc
+++ test/TestLedControl.cc
@@ -16,12 +16,41 @@ public:
 
 BOOST_FIXTURE_TEST_SUITE(ledcontrol_test_suite, TestLedControl)
 
-BOOST_AUTO_TEST_CASE(test_setcolor) {
-    setColor(0, 0, 0);
+BOOST_AUTO_TEST_CASE(test_setcolor_integer) {
+    BOOST_REQUIRE(setColor(1, 2, 3));
+    BOOST_CHECK_EQUAL(_rgbColor._red, 1);
+    BOOST_CHECK_EQUAL(_rgbColor._green, 2);
+    BOOST_CHECK_EQUAL(_rgbColor._blue, 3);
+}
+
+BOOST_AUTO_TEST_CASE(test_setcolor_integer_out_of_range) {
+    BOOST_CHECK_EQUAL(setColor(1, 256, 3), false);
     BOOST_CHECK_EQUAL(_rgbColor._red, 0);
     BOOST_CHECK_EQUAL(_rgbColor._green, 0);
     BOOST_CHECK_EQUAL(_rgbColor._blue, 0);
 }
+
+BOOST_AUTO_TEST_CASE(test_setcolor_string) {
+    BOOST_REQUIRE(setColor("1", "2", "3"));
+    BOOST_CHECK_EQUAL(_rgbColor._red, 1);
+    BOOST_CHECK_EQUAL(_rgbColor._green, 2);
+    BOOST_CHECK_EQUAL(_rgbColor._blue, 3);
+}
+
+BOOST_AUTO_TEST_CASE(test_setcolor_string_out_of_range) {
+    BOOST_CHECK_EQUAL(setColor("1", "256", "3"), false);
+    BOOST_CHECK_EQUAL(_rgbColor._red, 0);
+    BOOST_CHECK_EQUAL(_rgbColor._green, 0);
+    BOOST_CHECK_EQUAL(_rgbColor._blue, 0);
+}
+
+BOOST_AUTO_TEST_CASE(test_setcolor_string_invalid_number) {
+    BOOST_CHECK_EQUAL(setColor("1", "blabla", "3"), false);
+    BOOST_CHECK_EQUAL(_rgbColor._red, 0);
+    BOOST_CHECK_EQUAL(_rgbColor._green, 0);
+    BOOST_CHECK_EQUAL(_rgbColor._blue, 0);
+}
+
 
 
 BOOST_AUTO_TEST_SUITE_END()
